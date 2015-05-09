7085902eea429428f4b618bb2894cd1383f01aeb
Walter Firgau <walter.firgau@yahoo.com.au>
2015-05-09 20:02:59 +1000

    Second commit

--- plot1.R
+++ plot1.R
@@ -3,6 +3,6 @@ dataset <- subset(dataset, (Date == "1/2/2007") | (Date == "2/2/2007"))
 dataset$datetime <- strptime(paste(dataset$Date, dataset$Time), format="%d/%m/%Y %H:%M:%S")
 
 png("plot1.png", width=480, height=480)
-hist(dataset$Global_active_power, col ="red", xlab = "Global Active Power (kilowatts)", 
+hist(as.numeric(dataset$Global_active_power), col ="red", xlab = "Global Active Power (kilowatts)", 
      main = "Global Active Power")
 dev.off()
